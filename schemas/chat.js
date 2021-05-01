d5f945ff1000e6760cedd3d426bfc29db92ed80f
chaewonki <chaewk15@gmail.com>
2021-05-01 17:06:06 +0900

    mongoose basic setting
    1. add schema
    2. pug files -> chat layout main room

---
+++ schemas/chat.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const {
+  Types: { ObjectId },
+} = Schema; // ObjectId is from Room collection in Room Schema
+const chatSchema = new Schema({
+  room: {
+    type: ObjectId,
+    required: true,
+    ref: "Room",
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  chat: String,
+  gif: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
